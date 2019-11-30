eccb1f03805e735212a777508a38c4d1ce04a086
jpranay08 <54579190+jpranay08@users.noreply.github.com>
2019-11-30 16:28:24 +0530

    Update docker-image-build.bat

--- PyMicroservices-master/Source/docker-image-build.bat
+++ PyMicroservices-master/Source/docker-image-build.bat
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;C:\Users\Pranay Chowdary\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
-SET IMAGE_NAME=gcr.io/prabhacloud/gpymicro:%BuildNumber%
+SET IMAGE_NAME=gcr.io/prabhacloud/gpymicro:%BUILD_NUMBER%
 cd C:\Users\Pranay Chowdary\Desktop\PyMicroservices-master\Source
 gcloud builds submit --tag %IMAGE_NAME% .
